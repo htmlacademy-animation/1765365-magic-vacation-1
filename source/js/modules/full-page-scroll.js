085ec6873b5c5be53acd5e8aafeba363b62f3289
DmitriyWebDev <d-man_real_akkaunt.ru@mail.ru>
2022-05-07 15:45:08 +0600

    module1-task3. Откатил изменения файла - full-page-scroll.js к версии из ветки - module1-task2

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -3,12 +3,10 @@ import throttle from 'lodash/throttle';
 export default class FullPageScroll {
   constructor() {
     this.THROTTLE_TIMEOUT = 2000;
-    this.animatedTransitionScreensStartIndex = 1;
 
     this.screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
     this.menuElements = document.querySelectorAll(`.page-header__menu .js-menu-link`);
 
-    this.prevActiveScreen = 0;
     this.activeScreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
     this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
@@ -31,48 +29,14 @@ export default class FullPageScroll {
 
   onUrlHashChanged() {
     const newIndex = Array.from(this.screenElements).findIndex((screen) => location.hash.slice(1) === screen.id);
-    this.setActiveScreen(newIndex);
+    this.activeScreen = (newIndex < 0) ? 0 : newIndex;
     this.changePageDisplay();
   }
 
-  setActiveScreen(newIndex = 0) {
-    this.prevActiveScreen = this.activeScreen;
-    this.activeScreen = (newIndex < 0) ? 0 : newIndex;
-  }
-
   changePageDisplay() {
+    this.changeVisibilityDisplay();
     this.changeActiveMenuItem();
-
-    this.startScreenTransition(() => {
-      this.changeVisibilityDisplay();
-      this.emitChangeDisplayEvent();
-    });
-  }
-
-  startScreenTransition(completeScreenTransition) {
-    const transitionEndEventKey = `transitionend`;
-
-    const transitionBlock = document.getElementById(`screen-transition-block`);
-    const transitionActiveClassname = `screen-transition-animation-block--visible`;
-
-    const animatedStartIndex = this.animatedTransitionScreensStartIndex;
-    const shouldAnimateScreenTransition = this.prevActiveScreen <= animatedStartIndex && this.activeScreen > animatedStartIndex;
-
-    if (shouldAnimateScreenTransition) {
-      transitionBlock.classList.add(transitionActiveClassname);
-      transitionBlock.addEventListener(transitionEndEventKey, handleTransitionEnd);
-
-      return;
-    }
-
-    transitionBlock.classList.remove(transitionActiveClassname);
-    completeScreenTransition();
-
-    function handleTransitionEnd({target}) {
-      target.classList.remove(transitionActiveClassname);
-      completeScreenTransition();
-      transitionBlock.removeEventListener(transitionEndEventKey, handleTransitionEnd);
-    }
+    this.emitChangeDisplayEvent();
   }
 
   changeVisibilityDisplay() {
@@ -115,12 +79,10 @@ export default class FullPageScroll {
   }
 
   reCalculateActiveScreenPosition(delta) {
-    let currentActiveScreen = this.activeScreen;
-
     if (delta > 0) {
-      this.setActiveScreen(Math.min(this.screenElements.length - 1, ++currentActiveScreen));
+      this.activeScreen = Math.min(this.screenElements.length - 1, ++this.activeScreen);
     } else {
-      this.setActiveScreen(Math.max(0, --currentActiveScreen));
+      this.activeScreen = Math.max(0, --this.activeScreen);
     }
   }
 }
